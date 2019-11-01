d4290f2a9a680dcaa84373d29aa2433291abac51
Ali Haider <alihaider2011@live.ca>
2019-11-01 17:27:45 -0400

    Updates

---
+++ precache-manifest.b8bef3caef7ef1188a0e50f0b51ea625.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f424653a3402fc1927d305a677d9144",
+    "url": "/React-Tetris-Game/index.html"
+  },
+  {
+    "revision": "2bb0d91910684e06293c",
+    "url": "/React-Tetris-Game/static/css/main.5df1b16c.chunk.css"
+  },
+  {
+    "revision": "d4b8335ad76372dfd51f",
+    "url": "/React-Tetris-Game/static/js/2.4ae1bac8.chunk.js"
+  },
+  {
+    "revision": "2bb0d91910684e06293c",
+    "url": "/React-Tetris-Game/static/js/main.b9fe2c97.chunk.js"
+  },
+  {
+    "revision": "0bd78225f3e1a317b17a",
+    "url": "/React-Tetris-Game/static/js/runtime-main.fddf886a.js"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/React-Tetris-Game/static/media/Pixel-LCD-7.e2897552.woff"
+  },
+  {
+    "revision": "2810fe6b5435e75b3dad63e341b97d02",
+    "url": "/React-Tetris-Game/static/media/bg.2810fe6b.png"
+  }
+]);
